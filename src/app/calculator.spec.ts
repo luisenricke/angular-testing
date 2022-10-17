da366c0f959f79fdc67c2740c1b6e7766c8b33dc
Luis Villalobos <luisevm24@outlook.com>
2022-10-17 01:04:44 -0500

    refactor: better organization of calculadtor test

--- src/app/calculator.spec.ts
+++ src/app/calculator.spec.ts
@@ -1,40 +1,44 @@
 import { Calculator } from './calculator';
 
-fdescribe('Test for Calculator', () => {
-  it('#multiply should return a nine', () => {
-    // Arrange
-    const calculator = new Calculator();
-    // Act
-    const result = calculator.multiply(3, 3);
-    // Assert
-    expect(result).toEqual(9);
+describe('Test for Calculator', () => {
+  describe('Test for multiply', () => {
+    it('should return a nine', () => {
+      // Arrange
+      const calculator = new Calculator();
+      // Act
+      const result = calculator.multiply(3, 3);
+      // Assert
+      expect(result).toEqual(9);
+    });
+
+    it('should return a four', () => {
+      // Arrange
+      const calculator = new Calculator();
+      // Act
+      const result = calculator.multiply(1, 4);
+      // Assert
+      expect(result).toEqual(4);
+    });
   });
 
-  it('#multiply should return a four', () => {
-    // Arrange
-    const calculator = new Calculator();
-    // Act
-    const result = calculator.multiply(1, 4);
-    // Assert
-    expect(result).toEqual(4);
+  describe('Test for divide', () => {
+    it('should return a numbers', () => {
+      // Arrange
+      const calculator = new Calculator();
+      // Act & Assert
+      expect(calculator.divide(6, 3)).toEqual(2);
+      expect(calculator.divide(10, 2)).toEqual(5);
+    });
+
+    it('should return null with zero', () => {
+      // Arrange
+      const calculator = new Calculator();
+      // Act & Assert
+      expect(calculator.divide(2, 0)).toBeNull();
+    });
   });
 
-  it('#divide should return a numbers', () => {
-    // Arrange
-    const calculator = new Calculator();
-    // Act & Assert
-    expect(calculator.divide(6, 3)).toEqual(2);
-    expect(calculator.divide(10, 2)).toEqual(5);
-  });
-
-  it('#divide with zero', () => {
-    // Arrange
-    const calculator = new Calculator();
-    // Act & Assert
-    expect(calculator.divide(2, 0)).toBeNull();
-  });
-
-  it('test matchers', () => {
+  it('Test for matchers', () => {
     const name = 'Luis';
     let lastName;
 
