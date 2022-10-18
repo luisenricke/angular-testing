84d75dadf97cf4e973a0121cea6a3dea12080758
Luis Villalobos <luisevm24@outlook.com>
2022-10-18 00:17:03 -0500

    refacot: migrate MasterService into TestBet and SpyObj

--- src/app/services/master.service.spec.ts
+++ src/app/services/master.service.spec.ts
@@ -1,14 +1,32 @@
+import { TestBed } from '@angular/core/testing';
 import { MasterService } from './master.service';
 import { ValueService } from './value.service';
 import { FakeValueService } from './value-fake.service';
 
 describe('MasterService', () => {
-  let service: MasterService;
+  let masterService: MasterService;
+  let valueServiceSpy: jasmine.SpyObj<ValueService>;
 
   beforeEach(() => {
-    // service = new MasterService();
-  });
+    const valueServiceObjectSpy = jasmine.createSpyObj('ValueService', [
+      'getValue',
+    ]);
 
+    TestBed.configureTestingModule({
+      providers: [
+        MasterService,
+        { provide: ValueService, useValue: valueServiceObjectSpy },
+      ],
+    });
+    masterService = TestBed.inject(MasterService);
+    valueServiceSpy = TestBed.inject(
+      ValueService
+    ) as jasmine.SpyObj<ValueService>;
+  });
+  it('should be created', () => {
+    expect(masterService).toBeTruthy();
+  });
+  /*
   it('should be return "some value" from the other reference of service', () => {
     const valueService = new ValueService();
     const masterService = new MasterService(valueService);
@@ -28,11 +46,9 @@ describe('MasterService', () => {
     const masterService = new MasterService(fake as ValueService);
     expect(masterService.getValue()).toBe('fake from object');
   });
-
+ */
   it('should call to getValue from ValueService', () => {
-    const valueServiceSpy = jasmine.createSpyObj('ValueService', ['getValue']);
     valueServiceSpy.getValue.and.returnValue('fake value from spy');
-    const masterService = new MasterService(valueServiceSpy);
     expect(masterService.getValue()).toBe('fake value from spy');
     expect(valueServiceSpy.getValue).toHaveBeenCalled();
     expect(valueServiceSpy.getValue).toHaveBeenCalledTimes(1);
