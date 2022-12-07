4fbcc603c1555b0485538eeaa5fac5e425ef4164
Luis Villalobos <luisevm24@outlook.com>
2022-12-06 21:02:47 -0600

    feat: test service in compoentes and inputs

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -8,7 +8,7 @@ import { Product } from '../../models/product.model';
 })
 export class ProductComponent implements OnInit {
 
-  @Input() product!: Product;
+  @Input() product!: Product | null;
 
   constructor() { }
 
--- src/app/components/products/products.component.spec.ts
+++ src/app/components/products/products.component.spec.ts
@@ -1,25 +1,44 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 
+import { generateManyProducts } from '../../models/product.mock';
+import { ProductService } from '../../services/product.service';
+import { ProductComponent } from '../product/product.component';
 import { ProductsComponent } from './products.component';
 
-xdescribe('ProductsComponent', () => {
+describe('ProductsComponent', () => {
   let component: ProductsComponent;
   let fixture: ComponentFixture<ProductsComponent>;
+  let productService: jasmine.SpyObj<ProductService>;
 
   beforeEach(async () => {
+    const productServiceSpy = jasmine.createSpyObj('ValueService', ['getAll']);
+
     await TestBed.configureTestingModule({
-      declarations: [ ProductsComponent ]
+      declarations: [ProductsComponent, ProductComponent],
+      providers: [
+        { provide: ProductService, useValue: productServiceSpy },
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ProductsComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    productService = TestBed.inject(ProductService) as jasmine.SpyObj<ProductService>;
+
+
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should call to service', () => {
+    const productsMock = generateManyProducts(3);
+    productService.getAll.and.returnValue(of(productsMock));
+    fixture.detectChanges();
+    expect(productService.getAll).toHaveBeenCalled();
+  })
 });
